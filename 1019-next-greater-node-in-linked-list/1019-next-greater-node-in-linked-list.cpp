619410ecaa3302a120bc1ff7d8e65ba85099bb24
Rakshit Raj <rakshit.raj.cg@gmail.com>
2026-08-13 15:16:35 +0530

    Time: 461 ms (18.23%), Space: 45.7 MB (68.55%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -1,35 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> ans;
 
-        vector<int> answer;
-
-        ListNode* temp = head;
-
-        while (temp != NULL) {
-            answer.push_back(temp->val);
-            temp = temp->next;
+        while(head != nullptr){
+            ans.push_back(head->val);
+            head = head->next;
         }
 
-        for (int i = 0; i < answer.size(); i++) {
-
-            int original = answer[i];
-            bool found = false;
-
-            for (int j = i + 1; j < answer.size(); j++) {
-
-                if (answer[j] > original) {
-                    answer[i] = answer[j];
-                    found = true;
+        for(int i=0; i<ans.size(); i++){
+            int count = 0;
+            for(int j=i+1; j<ans.size(); j++){
+                if(ans[i] < ans[j]){
+                    ans[i] = ans[j];
+                    count = 1;
                     break;
                 }
             }
-
-            if (!found) {
-                answer[i] = 0;
+            if(count == 0){
+                ans[i] = 0;
             }
         }
 
-        return answer;
+
+        return ans;
     }
 };
