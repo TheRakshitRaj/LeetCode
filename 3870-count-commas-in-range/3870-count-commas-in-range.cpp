904b99786989f7b204d9c639a3bcf314eb4bddfe
Rakshit Raj <rakshit.raj.cg@gmail.com>
2026-09-08 10:54:46 +0530

    Time: 7 ms (10.12%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -3,8 +3,8 @@ public:
     int countCommas(int n) {
         int count = 0;
 
-        for (int i = 1; i <= n; i++) {
-            if (i >= 1000) {
+        for (int i = 1; i <= n; ++i) {
+            if (i > 999) {
                 count++;
             }
         }
