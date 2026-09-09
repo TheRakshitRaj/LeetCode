5509b8e1ffc9912f48def667d1f35e57e725761c
Rakshit Raj <rakshit.raj.cg@gmail.com>
2026-09-09 15:00:49 +0530

    Time: 0 ms (100%), Space: 11.8 MB (99.51%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
-        vector<int> ans;
-        int sum = 0;
+       vector <int> ans ;
 
-        for (int i = 0; i < operations.size(); i++) {
-            if (operations[i] == "C") {
-                ans.pop_back();
-            }
-            else if (operations[i] == "D") {
-                int n = ans.back() * 2;
-                ans.push_back(n);
-            }
-            else if (operations[i] == "+") {
-                int m = ans[ans.size()-1] + ans[ans.size()-2];
-                ans.push_back(m);
-            }
-            else {
-                ans.push_back(stoi(operations[i]));
-            }
-        }
+       int sum = 0 ;
 
-        for (int x : ans) sum += x;
-        return sum;
+       for(int i = 0 ; i < operations.size() ; i++){
+         if(operations[i] == "C"){
+            ans.pop_back() ;
+         }
+         else if(operations[i] == "D"){
+            int n = ans[ans.size()-1] * 2 ; 
+            ans.push_back(n) ; 
+         }
+        else if(operations[i] == "+"){
+            int m = ans[ans.size()-1] + ans[ans.size()-2] ; 
+            ans.push_back(m) ;  
+         }
+         else{
+            ans.push_back(stoi(operations[i])) ;
+         }
+       } 
+       for(int i = 0 ; i < ans.size() ; i++){
+          sum += ans[i] ;
+       }
+
+       return sum ;
     }
 };
